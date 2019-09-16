32b7ae451eb151ed84a91b01cc9e4be80d4ea442
Henrik Holm <henholm@kth.se>
2019-09-16 22:10:31 +0200

    Doing assignment 3

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -12,16 +12,18 @@ class Program:
         self.conn = pgdb.Connection(**params)
         self.conn.autocommit=False
         # specify the command line menu here
-        self.actions = [self.population_query, self.scatterplot_population, self.plot_datapoints_per_year, self.plot_mean_and_stdev_per_decade, self.exit]
+        self.actions = [self.population_query, self.scatterplot_population, self.plot_datapoints_per_year, self.plot_mean_and_stdev_per_decade, self.scatterplot_user_input, self.exit]
         # menu text for each of the actions above
-        self.menu = ["Population Query", "Population Scatterplot", "Plot # of datapoints per year", "Plot mean and std_dev per decade", "Exit"]
+        self.menu = ["Population Query", "Population Scatterplot", "Plot # of datapoints per year", "Plot mean and std_dev per decade", "Scatterplot data about your city", "Exit"]
         self.cur = self.conn.cursor()
 
 
     def print_menu(self, input_menu):
         """Prints a menu of all functions this program offers.  Returns the numerical correspondant of the choice made."""
+        print()
         for i, x in enumerate(input_menu):
             print("%i. %s"%(i+1, x))
+        print()
         return self.get_int(input_menu)
 
 
@@ -162,6 +164,85 @@ class Program:
             does not follow linear trend at all).
         """
         pass
+
+
+    def scatterplot_user_input(self):
+        """
+        Now create a text menu in python that asks a user a city and associated
+        country and creates a figure that displays all the population, year
+        datapoints for the city as a scatterplot with x-axis = year, y-axis =
+        population as well as predicted behavior of population as a line y =
+        ax + b on a graph.
+        """
+
+        cities_query = "SELECT DISTINCT city, country FROM citypops;"
+        self.cur.execute(cities_query)
+        cities = self.cur.fetchall()
+        cities = [t[0] for t in cities]
+        cities_lower = [c.lower() for c in cities]
+        user_city = input("\nPlease choose a city: ").lower().strip()
+        while user_city not in cities_lower:
+            user_city = input("\nPlease choose a valid city: ").lower().strip()
+
+        # seen = list()
+        # dups = list()
+        # for c in cities_lower:
+        #     if c in seen:
+        #         dups.append(c)
+        #     seen.append(c)
+        # print(dups)
+
+        country_query = "SELECT DISTINCT country FROM citypops WHERE city = '{}';".format(user_city.title())
+        self.cur.execute(country_query)
+        countries = self.cur.fetchall()
+        countries = [t[0] for t in countries]
+
+        if cities_lower.count(user_city) > 1:
+            print("\nThe city '{}' exists in the following countries: ".format(user_city.title()))
+            for i, c in enumerate(countries):
+                print("\t{}: {}".format(i+1, c))
+            user_country = input("\nPlease specify which country you mean: ").lower()
+            countries_lower = [c.lower() for c in countries]
+            while user_country not in countries_lower:
+                user_country = input("\nPlease choose a valid country: ").lower()
+        else:
+            user_country = countries[0]
+
+        user_country = user_country.upper()
+        user_city = user_city.title()
+
+        scatter_query = "SELECT year, population FROM citypops WHERE city = '{}' AND country = '{}'".format(user_city, user_country)
+        self.cur.execute(scatter_query)
+        years_pops = self.cur.fetchall()
+        years_pops = [(int(y_p[0]), int(y_p[1])) for y_p in years_pops]
+
+        years = list()
+        pops = list()
+        for y_p in years_pops:
+            print("Considering tuple", y_p)
+            if (y_p[0] != None and y_p[1] != None):
+                years.append(y_p[0])
+                pops.append(y_p[1])
+            else:
+                print("Dropped tuple ", y_p)
+
+
+        line_query = "SELECT a, b, yearfrom FROM LinearPrediction WHERE cityname = '{}' AND country = '{}';".format(user_city, user_country)
+        self.cur.execute(line_query)
+        a_b_yf = self.cur.fetchall()
+        a_b_yf = [(float(t[0]), float(t[1]), int(t[2])) for t in a_b_yf]
+        a_b_yf = a_b_yf[0]
+
+        a = a_b_yf[0]
+        b = a_b_yf[1]
+        yf = a_b_yf[2]
+
+        print(a, b, yf)
+
+        plt.scatter(years, pops)
+        plt.plot(years, a * years + b, '-')
+
+        plt.show()  # display figure if you run this code locally
 
 
     def exit(self):
