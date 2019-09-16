dbfcdaee63e1a6575c3bbe3b8c3aa60300059c2e
Henrik Holm <henholm@kth.se>
2019-09-16 12:05:00 +0200

    Solved assignment 1

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -14,15 +14,15 @@ class Program:
         self.conn = pgdb.Connection(**params)
         self.conn.autocommit=False
         # specify the command line menu here
-        self.actions = [self.population_query, self.exit]
+        self.actions = [self.population_query, self.population_scatterplot, self.exit]
         # menu text for each of the actions above
-        self.menu = ["Population Query", "Exit"]
+        self.menu = ["Population Query", "Population Scatterplot", "Exit"]
         self.cur = self.conn.cursor()
 
 
     def print_menu(self):
         """Prints a menu of all functions this program offers.  Returns the numerical correspondant of the choice made."""
-        for i,x in enumerate(self.menu):
+        for i, x in enumerate(self.menu):
             print("%i. %s"%(i+1,x))
         return self.get_int()
 
@@ -50,10 +50,43 @@ class Program:
         self.print_answer()
 
 
+    def l_of_tuples_to_l_of_lists(self, list_of_tuples):
+        xs = []
+        ys = []
+        for t in list_of_tuples:
+            print("Considering tuple", t)
+            if (t[0] != None and t[1] != None):
+                xs.append(t[0])
+                ys.append(t[1])
+            else:
+                print("Dropped tuple ", t)
+
+        return [xs, ys]
+
+
     def population_scatterplot(self):
-        query = "SELECT * FROM ".format()
-        [xs, ys] = query()
-        plt.scatter(xs, ys)
+        user_input = input("Per year or per decade? Y for year, D for decade. Your answer ([Y/D]): ")
+        print(user_input)
+        if user_input.lower() == "y":
+            select_1 = "year"
+            group_by = "year"
+        else:
+            select_1 = "(FLOOR(year/10)*10) AS decade"
+            group_by = "decade"
+
+
+        query = "SELECT {}, SUM(population) AS total_pop FROM citypops GROUP BY {} ORDER BY {};".format(select_1, group_by, group_by)
+        print("Will execute: ", query)
+
+        self.cur.execute(query)
+        # self.print_answer()
+
+        results = self.cur.fetchall()
+        results = [tuple(int(item) for item in t) for t in results]
+
+        xs_and_ys = self.l_of_tuples_to_l_of_lists(results)
+
+        plt.scatter(xs_and_ys[0], xs_and_ys[1])
         plt.show()  # display figure if you run this code locally
         plt.savefig("figure.png") # save figure as image in local directory
 
